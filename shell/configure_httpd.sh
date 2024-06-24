2805ce03102319a5a4f9d8321a5193ff4028d5a0
Alexandre SABRI <115658764+alexsabr@users.noreply.github.com>
2024-06-24 11:15:04 +0200

    WIP working on deploying the frontend on the same apache server as the backend, not yet working.

--- shell/configure_httpd.sh
+++ shell/configure_httpd.sh
@@ -3,15 +3,44 @@ set -eux
 
 
 echo "
-LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so 
+
+
+
+
+<VirtualHost *:80>
+	DocumentRoot "/root/yana-front/"
+	<Directory /root/yana-front>
+		Header set Access-Control-Allow-Origin *
+		AllowOverride all
+		Require all granted
+
+		Options indexes FollowSymLinks Multiviews
+
+		<IfModule mod_rewrite.c>
+		  RewriteEngine On
+		  RewriteBase /
+		  RewriteRule ^index.html$ - [L]
+		  RewriteCond %{REQUEST_FILENAME} !-f
+		  RewriteCond %{REQUEST_FILENAME} !-d
+		  RewriteRule . /index.html [L]
+		</IfModule>
+	</Directory>
+</VirtualHost>
+
+<VirtualHost *:81>
+<Directory /root/django-yana/yana>
+## Modules to make django work with apache
+LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so
 WSGIScriptAlias / /root/django-yana/yana/wsgi.py
 WSGIPythonHome /root/django-yana/.venv/
 WSGIPythonPath /root/django-yana
 WSGIApplicationGroup %{GLOBAL}
 
-<Directory /root/django-yana/yana>
 <Files wsgi.py>
-Header set Access-Control-Allow-Origin "*"
+Header set Access-Control-Allow-Origin *
 Require all granted
 </Files>
-</Directory>" >> "/usr/local/apache2/conf/httpd.conf"
+</Directory>
+</VirtualHost>
+
+" >> "/usr/local/apache2/conf/httpd.conf"
